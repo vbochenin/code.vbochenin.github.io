4e9aec46e8dcbc4d1c303e5131ed3a2537c5a64d
Vladislav Bochenin <vbochenin@example.com>
2022-11-14 11:52:49 +0100

    Adding constructor

--- src/main/java/io/github/vbochenin/BenchmarkRunner.java
+++ src/main/java/io/github/vbochenin/BenchmarkRunner.java
@@ -3,8 +3,24 @@ package io.github.vbochenin;
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BenchmarkRunner extends Runner {
+
+    private final Class<?> benchmarkClass;
+    private final List<Method> benchmarkMethods;
+
+    public BenchmarkRunner(Class<?> benchmarkClass) {
+        this.benchmarkClass = benchmarkClass;
+        this.benchmarkMethods = Arrays.stream(benchmarkClass.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(Benchmark.class))
+                .collect(Collectors.toList());
+    }
 
     @Override
     public Description getDescription() {
