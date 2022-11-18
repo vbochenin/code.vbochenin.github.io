58c3971fc40f147dcc44e47a3600af36718a767a
Vladislav Bochenin <vbochenin@example.com>
2022-11-18 22:51:57 +0100

    Filter benchmarks

--- src/main/java/io/github/vbochenin/BenchmarkRunner.java
+++ src/main/java/io/github/vbochenin/BenchmarkRunner.java
@@ -1,14 +1,10 @@
 package io.github.vbochenin;
 
-import java.lang.reflect.Method;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.Filterable;
+import org.junit.runner.manipulation.NoTestsRemainException;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -16,9 +12,19 @@ import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public class BenchmarkRunner extends Runner {
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BenchmarkRunner extends Runner implements Filterable {
     private final Class<?> benchmarkClass;
     private final List<Method> benchmarkMethods;
+
+    private List<Method> readyToRunMethods;
 
     public BenchmarkRunner(Class<?> benchmarkClass) {
         this.benchmarkClass = benchmarkClass;
@@ -30,7 +36,7 @@ public class BenchmarkRunner extends Runner {
     @Override
     public Description getDescription() {
         Description result = Description.createSuiteDescription(benchmarkClass);
-        benchmarkMethods.stream()
+        readyToRunMethods.stream()
                 .map(this::getBenchmarkMethodDescription)
                 .forEach(result::addChild);
         return result;
@@ -42,14 +48,14 @@ public class BenchmarkRunner extends Runner {
 
     @Override
     public void run(RunNotifier notifier) {
-        for (Method benchmarkMethod : benchmarkMethods) {
+        for (Method benchmarkMethod : readyToRunMethods) {
             Description testDescription = getBenchmarkMethodDescription(benchmarkMethod);
             try {
-            	notifier.fireTestStarted(testDescription);
+                notifier.fireTestStarted(testDescription);
                 Options opt = new OptionsBuilder()
                         .include(".*" + benchmarkClass.getName() + "." + benchmarkMethod.getName() + ".*")
                         .jvmArgsAppend("-Djmh.separateClasspathJAR=true")
-                        .result(benchmarkClass.getName() + "." + benchmarkMethod.getName()+"_"+LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ".json")
+                        .result(benchmarkClass.getName() + "." + benchmarkMethod.getName() + "_" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ".json")
                         .resultFormat(ResultFormatType.JSON)
                         .build();
 
@@ -61,5 +67,22 @@ public class BenchmarkRunner extends Runner {
                 return;
             }
         }
+    }
+
+    @Override
+    public void filter(Filter filter) throws NoTestsRemainException {
+        List<Method> filteredMethods = new ArrayList<>();
+
+        for (Method benchmarkMethod : benchmarkMethods) {
+            if (filter.shouldRun(getBenchmarkMethodDescription(benchmarkMethod))) {
+                filteredMethods.add(benchmarkMethod);
+            }
+        }
+
+        if (filteredMethods.isEmpty()) {
+            throw new NoTestsRemainException();
+        }
+
+        this.readyToRunMethods = filteredMethods;
     }
 }
