e5a10f11200b4e681db86e1af98d5a4377b66981
Vladislav Bochenin <vladislav.bochenin@siemens.com>
2025-01-31 11:55:42 +0100

    Fixes for hashmap errors

--- hashmap-error/README.md
+++ hashmap-error/README.md
@@ -1,1 +1,13 @@
 Code for the blog post 
+
+**How to build**
+
+``` bash
+mvn clean package
+```
+
+**How to run**
+
+``` bash
+java -jar target/hashmap-error.jar -t HashmapTest -v
+```
--- hashmap-error/src/main/java/io/github/vbochenin/HashmapTest.java
+++ hashmap-error/src/main/java/io/github/vbochenin/HashmapTest.java
@@ -6,23 +6,26 @@ import java.util.Objects;
 import java.util.Random;
 
 import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.Expect;
 import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
 import org.openjdk.jcstress.annotations.State;
 import org.openjdk.jcstress.infra.results.L_Result;
 
 @State
 @JCStressTest
-public class Main {
+@Outcome(id = "null",  expect = Expect.ACCEPTABLE, desc = "Boolean value is guarded")
+public class HashmapTest {
     public static final Random rnd = new Random();
-    Map<Integer, String> m = new HashMap<>();
+    public static final Map<Integer, String> m = new HashMap<>();
 
     @Actor
     public void mrFirst(L_Result r) {
-        r.r1 = Objects.requireNonNull(m.put(rnd.nextInt(), "Message from Mr. First"));
+        r.r1 = m.put(rnd.nextInt(), "Message from Mr. First");
     }
 
     @Actor
     public void mrSecond(L_Result r) {
-        r.r1 = Objects.requireNonNull(m.put(rnd.nextInt(), "Message from Mr. Second"));
+        r.r1 = m.put(rnd.nextInt(), "Message from Mr. Second");
     }
 }
